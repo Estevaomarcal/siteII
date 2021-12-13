e927c8e46080d1ba0749b576ae2ecc4f3ff0e1c4
Estevaomarcal <91230989+Estevaomarcal@users.noreply.github.com>
2021-12-13 19:04:33 -0300

    Add files via upload

---
+++ Abis-Argan/script.js
@@ -0,0 +1,6 @@
+let nome = prompt("Qual é seu nome ?")
+let pesquisa = prompt("Você conhece a marca Abís ?")
+let pesquisa2 = prompt("Você gostaria de conhecer ?")
+let dados = parseInt(prompt("Digite seu numero de watzapp"))
+
+alert("Abís & você agradece sua atenção!")
